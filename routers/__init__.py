43270667bb29eda2665f19afeb57535c5c7c7751
Josue C Pinto Nt <josuecpn93@gmail.com>
2026-07-31 17:06:43 -0300

    refactor: reorganize router structure by removing old files and adding new ones for stars, planets, and meteors

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,6 +1,6 @@
-from .estrela import router as star_router
-from .planeta import router as planet_router
-from .meteoro import router as meteor_router
+from .star import router as star_router
+from .planet import router as planet_router
+from .meteor import router as meteor_router
 from .nasa import router as nasa_router
 from .analytics import router as analytics_router
 from .physics import router as physics_router
